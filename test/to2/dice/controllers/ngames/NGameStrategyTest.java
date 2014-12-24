e127b843f5c2799f77a1c378c1d97a4c906ab332
Redid <dwdsuder@gmail.com>
2014-12-24 16:11:17 +0100

    testFinishGame for NGame

--- test/to2/dice/controllers/ngames/NGameStrategyTest.java
+++ test/to2/dice/controllers/ngames/NGameStrategyTest.java
@@ -2,31 +2,32 @@ package to2.dice.controllers.ngames;
 
 import org.junit.Before;
 import org.junit.Test;
+import to2.dice.controllers.GameController;
 import to2.dice.controllers.GameStrategy;
 import to2.dice.controllers.ngames.strategies.PlusCountingStrategy;
 import to2.dice.game.*;
 
 import java.lang.reflect.Type;
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 public class NGameStrategyTest {
+    private Player[] players = {new Player("first", false, 5), new Player("second", false, 5)};
+    private NGameState state;
+
+    @Before
+    public void setUp() throws Exception{
+        state = new NGameState();
+    }
 
     @Test
     public void testStartGame() throws Exception {
         System.out.println("TESTING GAME LOGIC IN NGAMESTRATEGY");
-        NGameState state = new NGameState();
-        Player[] players = new Player[2];
-        players[0] = new Player("first", false, 5);
-        players[1] = new Player("second", false, 5);
         state.addPlayer(players[0]);
         state.addPlayer(players[1]);
 
-        GameSettings settings = new GameSettings(GameType.NPLUS, 5, "test", 1, 10, 2, 2, new HashMap<BotLevel, Integer>());
+        GameSettings settings = new GameSettings(GameType.NPLUS, 5, "test", 2, 10, 2, 2, new HashMap<BotLevel, Integer>());
         CountingStrategy countingStrategy = new PlusCountingStrategy();
         GameStrategy gameStrategy = new NGameStrategy(settings, state, countingStrategy);
 
@@ -36,14 +37,15 @@ public class NGameStrategyTest {
         assertTrue("Game should be started", state.isGameStarted());
         assertTrue("It should be first round", state.getCurrentRound() == 1);
 
-        assertTrue("It should be first player playing", state.getCurrentPlayer() == players[0]);
+        int curr = 0;
+        while(players[curr] != state.getCurrentPlayer()) curr++;
 
-        int[] currDice = players[0].getDice().getDiceArray().clone();
+        int[] currDice = players[curr].getDice().getDiceArray().clone();
         boolean[] chosenDice = new boolean[settings.getDiceNumber()];
         java.util.Arrays.fill(chosenDice, false);
         chosenDice[0] = true;
         gameStrategy.reroll(chosenDice);
-        int[] newDice = players[0].getDice().getDiceArray();
+        int[] newDice = players[curr].getDice().getDiceArray();
         boolean diceDiff = true;
         for(int i = 0; i<settings.getDiceNumber(); i++){
             if(chosenDice[i]){
@@ -62,6 +64,42 @@ public class NGameStrategyTest {
 
         assertTrue("Only first dice should be different", diceDiff);
 
-        assertTrue("It should be second player playing", state.getCurrentPlayer() == players[1]);
+        if(curr == 0) curr = 1;
+        else curr = 0;
+        assertTrue("It should be next player, after current, playing", state.getCurrentPlayer() == players[curr]);
+    }
+
+    @Test
+    public void finishGame() throws Exception{
+        System.out.println("TESTING NGAMESTRATEGY ENDING GAME");
+        state.addPlayer(players[0]);
+
+        GameSettings settings = new GameSettings(GameType.NPLUS, 5, "test", 1, 10, 2, 1, new HashMap<BotLevel, Integer>());
+
+        /* creating new Counting strategy without random */
+        CountingStrategy countingStrategy = new CountingStrategy() {
+            @Override
+            public int countPoints(Dice dice) {
+                return 5;
+            }
+
+            @Override
+            public int generateWinningNumber(int diceNumber) {
+                return 6;
+            }
+        };
+
+        GameStrategy gameStrategy = new NGameStrategy(settings, state, countingStrategy);
+        gameStrategy.startGame();
+        assertTrue("Game should be started", state.isGameStarted());
+
+        /* Setting sure win number */
+        state.setWinningNumber(5);
+
+
+        boolean[] chosenDice = new boolean[settings.getDiceNumber()];
+        java.util.Arrays.fill(chosenDice, false);
+        gameStrategy.reroll(chosenDice);
+        assertTrue("Game should end", !state.isGameStarted());
     }
 }
